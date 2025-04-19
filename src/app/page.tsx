535e7481f903f1ffff9485fa5dbbcad74f785b9c
Artem Chernii <artemchernii@unknownc643426ecd46.lan>
2025-04-19 23:48:44 +0100

    refactor(page): remove unused Image import and update console log
    The Image import was unused and has been removed to clean up the code. The console log has been updated to include a descriptive label for better debugging clarity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import { Suspense } from "react";
-import Image from "next/image";
 import { db } from "~/server/db";
 
 export default async function HomePage() {
   const data = await db.query.posts.findMany();
-  console.log(data)
+  console.log(data, "DATA");
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
